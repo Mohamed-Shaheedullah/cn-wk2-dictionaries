2ba11e439116b688d3cff175adaffb27252a414f
Mohamed Shaheedullah <conifer567@outlook.com>
2024-02-06 15:46:17 +0000

    challenges commented

--- wk_2_day_2_challenge_4.py
+++ wk_2_day_2_challenge_4.py
@@ -14,12 +14,19 @@ animals = {
 input_animal = input("Enter an animal name   ")
 input_baby = input("Enter a baby name   ")
 
+### to test if we have completed loop and not found the animal in the list
 Existsflag = False
+
+### loop through keys of dictionary, i will represent key
 
 for i in animals:
     if i == input_animal:
         Existsflag=True
         print(animals.get(i))
+
+
+### this is out of the if and the loop, so if flag is false we have not found animal
+### we can the add the animal as per instructions        
 
 if Existsflag == False:
     animals.update({input_animal : input_baby})
